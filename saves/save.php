f9345b104e796046a99f83952e4c2bc755abe9c4
codetheuniverse <jsduncan98@gmail.com>
2013-03-12 16:06:03 -0500

    Updated the save.php file to use database.
    FIle contents are stored in a static filename.code and the file details
    are stored in the database for quick access.

--- saves/save.php
+++ saves/save.php
@@ -1,16 +1,35 @@
 <?php 
+
+	// Setup variables
 	$filename = $_POST['filename'];
 	$extension = $_POST['extension'];
 	$content = $_POST['contents'];
-	$time = null; // get time stamp
-	
-	if($_POST['author'] != "") {$author = $_POST['author'];}
-	if($_POST['title'] != "") {$title = $_POST['title'];}
-	if($_POST['key'] != "") {$password = $_POST['key'];}
+	$date = date('Y-m-d H:i:s'); // get time stamp
+	if($_POST['author'] != "") {$author = $_POST['author'];} else {$author = ""};
+	if($_POST['title'] != "") {$title = $_POST['title'];} else {$title = ""};
+	if($_POST['key'] != "") {$password = $_POST['key'];} else {$key = ""};
 
-	file_put_contents($filename . ".code" , $content); 
+	// Setup database connection
+	$dbname='base';
+	$mytable ="file_store";
+	$base=new SQLite3($dbname, 0666);
 
-	$query = "INSERT INTO $mytable(ID, post_title, post_content, post_author, post_date, guid)
-                VALUES ('$number', '$title', '$content', '$author', '$date', '$url')";
+	// Do the query
+	$query = "SELECT * FROM $mytable WHERE ID = $filename";
+	$row = $results->fetchArray();
 	$results = $base->exec($query);
+	if(count($row) > 0) {
+		$query = "	UPDATE $mytable 
+					SET code_title=$title, code_extension=$extension, code_author=$author, code_date=$date, code_password=$key 
+					WHERE ID=$filename
+				 ";
+		$results = $base->exec($query);
+	} else {
+		$query = "INSERT INTO $mytable(ID, code_title, code_extension, code_author, code_date, code_password)
+                VALUES ('$filename', '$title', '$extension', '$author', '$date', '$key')";
+		$results = $base->exec($query);
+	}
+
+	// Save the actual file
+	file_put_contents($filename . ".code" , $content); 
 ?>
